0ee4a0317697402f210b426c67fcf177f04645fc
DevManTowhid <mdtowhidulalam364@gmail.com>
2026-05-11 18:39:53 +0600

    Update Next.js configuration options
    Added output and basePath configurations, and set images to unoptimized.

--- learn-with-towhid-frontend/next.config.ts
+++ learn-with-towhid-frontend/next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/learn-with-towhid',
+  images: {
+    unoptimized: true,
+  },
+  // Optional: If you have persistent type errors you want to ignore to force a deploy
+  // typescript: { ignoreBuildErrors: true },
 };
 
 export default nextConfig;
